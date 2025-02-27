d7f69cf58c2035393cc2c64213b8b5da2d87be14
Kimhar00 <kimnassarlim991@gmail.com>
2025-02-28 00:25:24 +0800

    Replaced all files with the latest versions

--- .history/src/Hero_20250228001018.jsx
+++ .history/src/Hero_20250228001018.jsx
@@ -9,7 +9,7 @@ const Hero = () => {
         <h1>Aiyub, muhammad Husayn</h1>
         <p>Bachelor of science in Information Technology</p>
         <div className="photo">
-        <img src={myPhoto} alt="Sayn" />
+        <img src="/sayana.jpg" alt="My Photo" />
         </div>
     </div>
 </div>
